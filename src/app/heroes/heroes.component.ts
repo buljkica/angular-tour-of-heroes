7cec88bd74605fb89f775dd64762bfbed865d5e4
Igor Buljan <buljan.igor@gmail.com>
2019-04-03 16:02:17 +0200

    Updated hero listing and details part of the view.
    End of https://angular.io/tutorial/toh-pt2 tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { OnInit, Component } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,13 +9,13 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 }
